193b26297302c567d5a517be7c4527d6961d8ed5
thomas <>
2007-01-30 19:04:53 +0000

    add some rel_urn and So urns for expanded API tests.

--- src/test/java/net/datamodel/soml/BaseCase.java
+++ src/test/java/net/datamodel/soml/BaseCase.java
@@ -16,7 +16,11 @@ public class BaseCase  extends TestCase {
 	
 	protected URN urn1 = null;
 	protected URN urn2 = null;
+	protected URN urn3 = null;
+	protected URN urn4 = null;
 	protected URN rel_urn = null;
+	protected URN rel_urn2 = null;
+	protected URN rel_urn3 = null;
 
 	@Override
 	protected void setUp() throws Exception {
@@ -28,7 +32,11 @@ public class BaseCase  extends TestCase {
 		try {
 			urn1 = new URNImpl("urn:SemanticObject1");
 			urn2 = new URNImpl("urn:SemanticObject2");
+			urn3 = new URNImpl("urn:SemanticObject3");
+			urn4 = new URNImpl("urn:SemanticObject4");
 			rel_urn = new URNImpl("urn:rel1");
+			rel_urn = new URNImpl("urn:rel2");
+			rel_urn = new URNImpl("urn:rel3");
 		} catch (Exception e) {
 			fail ("Test Setup Error:"+e.getMessage());
 			throw e;
