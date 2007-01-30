1c4b0b9fcfdb4bd3a655d60b0680881d41989ea0
thomas <>
2007-01-30 19:04:23 +0000

    initial commit for future serialization tests.

---
+++ src/test/java/net/datamodel/soml/core/TestSerialization.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package net.datamodel.soml.core;
+
+import net.datamodel.soml.BaseCase;
+
+/**
+ * @author thomas
+ *
+ */
+public class TestSerialization extends BaseCase {
+	
+	public void test1() {
+		assertTrue(true);
+	}
+
+}
