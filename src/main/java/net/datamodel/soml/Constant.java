f50222134b75874ee806bcf1859b499b868f36a3
thomas <>
2007-03-08 00:49:36 +0000

    XML namespace stuff in XSSP package now

--- src/main/java/net/datamodel/soml/Constant.java
+++ src/main/java/net/datamodel/soml/Constant.java
@@ -8,14 +8,6 @@ public class Constant {
 	 */ 
 	public static final String SOML_NAMESPACE_URI = "http://www.data-model.net/SOML";
 
-	/** The XML schema version used by this package.
-	 */ 
-	public static final String XML_SCHEMA_NAMESPACE_URI = "http://www.w3.org/2001/XMLSchema";
-	
-	/** The XML schema-instance version used by this package.
-	 */ 
-	public static final String XML_SCHEMA_INSTANCE_NAMESPACE_URI = "http://www.w3.org/2001/XMLSchema-instance"; 
-	
 	/** The name of the relevant version of the schema file for this package.
 	 */ 
 	public static final String SOML_SCHEMA_NAME = "SOML_01.xsd";
