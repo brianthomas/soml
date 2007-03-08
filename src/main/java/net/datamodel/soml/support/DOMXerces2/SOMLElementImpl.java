3516e9187ae46765426b565b207855cddc49f547
thomas <>
2007-03-08 21:01:09 +0000

    remove bad constructor, fix indentation

--- src/main/java/net/datamodel/soml/support/DOMXerces2/SOMLElementImpl.java
+++ src/main/java/net/datamodel/soml/support/DOMXerces2/SOMLElementImpl.java
@@ -31,19 +31,13 @@
 package net.datamodel.soml.support.DOMXerces2;
 
 import java.io.IOException;
-import java.io.Writer;
-import java.util.Hashtable;
 
 import net.datamodel.soml.SemanticObject;
 import net.datamodel.soml.support.SOMLElement;
-import net.datamodel.xssp.XMLSerializableField;
-import net.datamodel.xssp.XMLSerializableObject;
-import net.datamodel.xssp.support.Constants;
+import net.datamodel.xssp.parse.DOMXerces2.XSSPElementImpl;
 
 import org.apache.log4j.Logger;
 import org.apache.xerces.dom.DocumentImpl;
-import org.apache.xerces.dom.ElementNSImpl;
-import org.w3c.dom.Attr;
 import org.w3c.dom.Comment;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Node;
@@ -56,158 +50,146 @@ public class SOMLElementImpl extends XSSPElementImpl
 implements SOMLElement
 {
 	
-	
-   // hmm..badness. We expect SemanticObject interface, then demand (internally) the 
-   // XMLSerializableObject interface. This could lead to problems down the
-   // line. :P 
-   public SOMLElementImpl (String namespaceURI, SemanticObject SemanticObject, DocumentImpl doc) 
-   throws IOException,NullPointerException
-   {
-      super (doc, namespaceURI, ((XMLSerializableObject) SemanticObject).getXMLNodeName());
-      ((XMLSerializableObject) SemanticObject).setNamespaceURI(namespaceURI);
-      setSemanticObject(SemanticObject);
-   }
+	private static final Logger logger = Logger.getLogger(SOMLElementImpl.class);
 
-   // this has same issue as the constructor above
-   public SOMLElementImpl (SemanticObject SemanticObject, DocumentImpl doc) 
-   throws IOException,NullPointerException
-   {
-      super (doc, Constants.SOML_NAMESPACE_URI, ((XMLSerializableObject) SemanticObject).getXMLNodeName());
-      setSemanticObject(SemanticObject);
-   }
+	// this has same issue as the constructor above
+	public SOMLElementImpl (SemanticObject so, DocumentImpl doc) 
+	throws IOException,NullPointerException
+	{
+		super (so, doc);
+		setSemanticObject(so);
+	}
 
-   //
-   // Get/Set Methods 
-   //
+	//
+	// Get/Set Methods 
+	//
 
-   public SemanticObject getSemanticObject() 
-   {
-	   return (SemanticObject) getObject();
-   }
+	public SemanticObject getSemanticObject() {
+		return (SemanticObject) getUserData();
+	}
 
-   public void setSemanticObject (SemanticObject object) 
-   throws NullPointerException
-   {
-	   setObject(object);
-   }
+	public void setSemanticObject (SemanticObject o) 
+	throws NullPointerException {
+		setUserData(o);
+	}
 
-   //
-   // Other Public Methods
-   //
+	//
+	// Other Public Methods
+	//
 
-   /** */
-   @Override
-   public Node appendChild(Node newChild) 
-   throws DOMException
-   {
+	/** */
+	@Override
+	public Node appendChild(Node newChild) 
+	throws DOMException
+	{
 
-      if (newChild instanceof SOMLElement) 
-      {
-         SOMLElement soElem = (SOMLElement) newChild;
-         SemanticObject so = soElem.getSemanticObject();
+		if (newChild instanceof SOMLElement) 
+		{
+			SOMLElement soElem = (SOMLElement) newChild;
+			SemanticObject so = soElem.getSemanticObject();
 
-         // Add as a member
-         // TODO : add relationship
-         getSemanticObject().addMember(so);
+			// Add as a member
+			// TODO : add relationship
+//			getSemanticObject().addMember(so);
 
-         newChild = super.appendChild(newChild); // needed? 
+			newChild = super.appendChild(newChild); // needed? 
 
-      } else if (newChild instanceof Comment) 
-      {
-          logger.warn("Can't append comment node into SOMLElement..ignoring.");
-          return null;
-      } else {
-         String tag = newChild.getNodeName();
-         throw new DOMException (DOMException.NOT_SUPPORTED_ERR, "Can't append regular DOM Element:"+tag+" to SOMLElement object:"+this.getNodeName()+".");
-      }
+		} else if (newChild instanceof Comment) 
+		{
+			logger.warn("Can't append comment node into SOMLElement..ignoring.");
+			return null;
+		} else {
+			String tag = newChild.getNodeName();
+			throw new DOMException (DOMException.NOT_SUPPORTED_ERR, "Can't append regular DOM Element:"+tag+" to SOMLElement object:"+this.getNodeName()+".");
+		}
 
-      return newChild;
-   }
+		return newChild;
+	}
 
-   /** */
-   @Override
-   public Node insertBefore(Node newChild, Node refChild) throws DOMException
-   {
+	/** */
+	@Override
+	public Node insertBefore(Node newChild, Node refChild) throws DOMException
+	{
 
-      Node node = null;
+		Node node = null;
 
-      if (newChild instanceof SOMLElement)
-      {
-         SOMLElement soElem = (SOMLElement) newChild;
-         SemanticObject so = soElem.getSemanticObject();
+		if (newChild instanceof SOMLElement)
+		{
+			SOMLElement soElem = (SOMLElement) newChild;
+			SemanticObject so = soElem.getSemanticObject();
 
-         // Add as a member
-         // TODO: add relationship
-         getSemanticObject().addMember(so);
+			// Add as a member
+			// TODO: add relationship
+//			getSemanticObject().addMember(so);
 
-         node = super.insertBefore(newChild,refChild);
+			node = super.insertBefore(newChild,refChild);
 
-      } else {
-         throw new DOMException (DOMException.NOT_SUPPORTED_ERR, "Can't remove regular DOM Element from SOMLElement object.");
-      }
+		} else {
+			throw new DOMException (DOMException.NOT_SUPPORTED_ERR, "Can't remove regular DOM Element from SOMLElement object.");
+		}
 
-      return node;
-   }
+		return node;
+	}
 
-   /** */
-   @Override
-   public Node removeChild(Node oldChild) 
-   throws DOMException
-   {
+	/** */
+	@Override
+	public Node removeChild(Node oldChild) 
+	throws DOMException
+	{
 
-      if (oldChild instanceof SOMLElement)
-      {
-         SOMLElement soElem = (SOMLElement) oldChild;
-         SemanticObject so = soElem.getSemanticObject();
+		if (oldChild instanceof SOMLElement)
+		{
+			SOMLElement soElem = (SOMLElement) oldChild;
+			SemanticObject so = soElem.getSemanticObject();
 
-         // Add as a member
-         // TODO: remove relationship (!)
-         getSemanticObject().removeMember(so);
+			// Add as a member
+			// TODO: remove relationship (!)
+//			getSemanticObject().removeMember(so);
 
-         oldChild = super.removeChild(oldChild); 
+			oldChild = super.removeChild(oldChild); 
 
-      } else {
-         throw new DOMException (DOMException.NOT_SUPPORTED_ERR, "Can't remove regular DOM Element from SOMLElement object.");
-      }
+		} else {
+			throw new DOMException (DOMException.NOT_SUPPORTED_ERR, "Can't remove regular DOM Element from SOMLElement object.");
+		}
 
-      return oldChild;
-   }
+		return oldChild;
+	}
 
-   /** */
-   @Override
-   public Node replaceChild(Node newChild, Node oldChild) 
-   throws DOMException
-   {
-       Node node = super.replaceChild(newChild, oldChild);
+	/** */
+	@Override
+	public Node replaceChild(Node newChild, Node oldChild) 
+	throws DOMException
+	{
+		Node node = super.replaceChild(newChild, oldChild);
 
-       if (oldChild instanceof SOMLElement)
-       {
-          SOMLElement soElem = (SOMLElement) oldChild;
-          SemanticObject so = soElem.getSemanticObject();
+		if (oldChild instanceof SOMLElement)
+		{
+			SOMLElement soElem = (SOMLElement) oldChild;
+			SemanticObject so = soElem.getSemanticObject();
 
-          // remove member
-         // TODO: remove relationship (!)
-          getSemanticObject().removeMember(so);
+			// remove member
+			// TODO: remove relationship (!)
+//			getSemanticObject().removeMember(so);
 
-       } else {
-         throw new DOMException (DOMException.NOT_SUPPORTED_ERR, "Can't insertBefore w/ DOM Element in SOMLElement object.");
-       }
+		} else {
+			throw new DOMException (DOMException.NOT_SUPPORTED_ERR, "Can't insertBefore w/ DOM Element in SOMLElement object.");
+		}
 
-       if (newChild instanceof SOMLElement)
-       {
-          SOMLElement soElem = (SOMLElement) newChild;
-          SemanticObject so = soElem.getSemanticObject();
+		if (newChild instanceof SOMLElement)
+		{
+			SOMLElement soElem = (SOMLElement) newChild;
+			SemanticObject so = soElem.getSemanticObject();
 
-          // add member
-         // TODO: add relationship (!)
-          getSemanticObject().addMember(so);
+			// add member
+			// TODO: add relationship (!)
+//			getSemanticObject().addMember(so);
 
-       } else {
-         throw new DOMException (DOMException.NOT_SUPPORTED_ERR, "Can't insertBefore w/ DOM Element in SOMLElement object.");
-       }
+		} else {
+			throw new DOMException (DOMException.NOT_SUPPORTED_ERR, "Can't insertBefore w/ DOM Element in SOMLElement object.");
+		}
 
-       return node;
-   }
+		return node;
+	}
 
 }
 
