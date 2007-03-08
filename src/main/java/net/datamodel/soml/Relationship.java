eb5f43c2cb9c09e10488fe1d3edb462ad9bea3d9
thomas <>
2007-03-08 21:17:09 +0000

    update documentation

--- src/main/java/net/datamodel/soml/Relationship.java
+++ src/main/java/net/datamodel/soml/Relationship.java
@@ -5,14 +5,20 @@ import java.net.URI;
 
 import net.datamodel.xssp.XMLSerializableObject;
 
-/** Models a uni-directional relationship between 2 SemanticObjects.
+/** <p>Models a uni-directional relationship between 2 SemanticObjects.
  * One object is the 'owner' (or caller) and the other is the target
  * SO, to which the relationship 'points to'.
+ * </p>
  *  
+ * <p>A Note on interface extension: Making this XMLSerializableObject instead of XMLSerializableObjectWithFields
+ * is intentional, in this manner we control/prohibit the user from adding any
+ * more fields to this object type which seems to make sense, at this time :).
+ * </p>
+ * 
  * @author thomas
- *
  */
-public interface Relationship extends XMLSerializableObject {
+public interface Relationship extends XMLSerializableObject 
+{
 	
 	/** Find the target SemanticObject of the relationship.
 	 * 
