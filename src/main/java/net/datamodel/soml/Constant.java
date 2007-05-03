9879fbb0e1f8b6531b367b9dcaef9e0bc27695f2
thomas <>
2007-05-03 18:44:40 +0000

    bump up schema version, change the uri from .../SOML to ../SemanticObject

--- src/main/java/net/datamodel/soml/Constant.java
+++ src/main/java/net/datamodel/soml/Constant.java
@@ -6,11 +6,11 @@ public class Constant {
 	
 	/** The namespace URI of this package.
 	 */ 
-	public static final String SOML_NAMESPACE_URI = "http://www.data-model.net/SOML";
+	public static final String SOML_NAMESPACE_URI = "http://www.data-model.net/SemanticObject";
 
 	/** The name of the relevant version of the schema file for this package.
 	 */ 
-	public static final String SOML_SCHEMA_NAME = "SOML_54.xsd";
+	public static final String SOML_SCHEMA_NAME = "SOML_55.xsd";
 	
 	/** The name of the attribute which stores the URI in the XML 
 	 * serialization of instances of SemanticObject, Property and
