784aa6a3ed433d5eb3c5837a679a9bb7d31c3674
thomas <>
2007-11-29 20:21:52 +0000

    Fix bug in SemObjBuilder...we should be counting sub-classes, not supers. Have the test load data from SDR package (so also add dependency in pom). This will cause  a circular dependency eventually when we want to alter the SDR interface to feed back SO's...what this means is that we will have to revisit this issue (soon), but it works right now.

--- src/main/java/net/datamodel/soml/builder/SemanticObjectBuilder.java
+++ src/main/java/net/datamodel/soml/builder/SemanticObjectBuilder.java
@@ -14,12 +14,20 @@ import net.datamodel.soml.impl.SemanticObjectImpl;
 import org.apache.log4j.Logger;
 
 import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
 import com.hp.hpl.jena.vocabulary.OWL;
 import com.hp.hpl.jena.vocabulary.RDF;
 
-/**
+/** Build SemanticObjects from the indicated individuals using the
+ * indicated OntModel for guidance. If the OntModel has provisions
+ * for multiple inheritance for a given class, then the builder should
+ * <b>not</b> have special handlers created for classes at the point in the class
+ * tree where the hierarchy joins a subclass from 2 or more superclasses.  
+ * 
  * @author thomas
  *
  */
@@ -30,20 +38,25 @@ public class SemanticObjectBuilder
 	private static final Logger logger = Logger.getLogger(SemanticObjectBuilder.class);
 	
 	private static final String RDFTypeURI = RDF.getURI()+"type";
+	private static final String OWLThingURI = OWL.getURI()+"Thing";
 	private static final String OWLSameAsURI = OWL.getURI()+"sameAs";
 	
 	private Map<String,SemanticObjectHandler> handlers = new Hashtable<String,SemanticObjectHandler>(); 
 	private SemanticObjectHandler DefaultHandler = new DefaultHandler();
+	private OntModel ontModel = null;
+	private Map<String,Integer> numOfSubClasses = new Hashtable<String,Integer>();
 	
-	public SemanticObjectBuilder() { }
+	public SemanticObjectBuilder (OntModel model) { 
+		ontModel = model; 
+	}
 	
-	/** Add a special handler for particular URI.
+	/** Add a special handler for particular rdf:type.
 	 * 
 	 * @param uri
 	 * @param handler
 	 */
-	protected void addHandler(String uri, SemanticObjectHandler handler) {
-		handlers.put(uri,handler);
+	protected void addHandler(String rdfTypeUri, SemanticObjectHandler handler) {
+		handlers.put(rdfTypeUri,handler);
 	}
 	
 	/** Find the handler associated with the indicated uri.
@@ -51,9 +64,9 @@ public class SemanticObjectBuilder
 	 * @param uri
 	 * @return
 	 */
-	protected SemanticObjectHandler findHandler (String uri) {
-		if(handlers.containsKey(uri))
-			return handlers.get(uri);
+	protected SemanticObjectHandler findHandler (String rdfTypeUri) {
+		if(handlers.containsKey(rdfTypeUri))
+			return handlers.get(rdfTypeUri);
 		else
 			return DefaultHandler;
 	}
@@ -71,13 +84,65 @@ public class SemanticObjectBuilder
 		String uri = in.getURI();
 		logger.debug("createSemanticObject (uri:"+uri+")");
 		
-		// create the SO
-		SemanticObject so = findHandler(uri).create(this,in);
+		// create the SO using the desired rdf:type 
+		SemanticObject so = findHandler(findRDFType(in)).create(this,in);
 		
 		return so;
 		
 	}
 
+	/** Look for the most specific rdf:type (e.g. the one which
+	 * has the most superclasses) of a given Individual. Problems
+	 * will arise if the OntModel allows for multiple inheritance
+	 * trees, but as this is bad practice, we ignore this issue for
+	 * now.
+	 */
+	private String findRDFType (Individual in) {
+		
+		String type = OWLThingURI;
+		int numOfSubClasses = 1000000; // pick something arbitrarily high
+		
+		// look for rdf:type properties...then rank them
+		// according to how many superclasses each has
+		for(StmtIterator i = in.listProperties(); i.hasNext(); ) {
+			Statement s  = i.nextStatement(); 
+			if (s.getPredicate().getURI().equals(RDFTypeURI)) {
+				String typeUri = s.getObject().toString();
+				logger.debug(" FOUND class rdf:type:"+typeUri);
+				logger.debug("     vs owl uri:"+OWLThingURI);
+				// only consider non owl:Thing rdf:type's as we
+				// have owl:Thing as our default.
+				if (!typeUri.equals(OWLThingURI)) {
+					logger.debug(" IN non-Thing conditional ");
+					OntClass oc = ontModel.getOntClass(typeUri);
+					logger.debug(" FOUND Class match oc:"+oc);
+					if (oc != null) {
+						int num_subs = findNrofSubClasses(oc);
+						logger.debug("** class uri:"+typeUri+" has "+num_subs+" subclasses");
+						// if this new class has more super classes, then
+						// it is preferable 
+						if (num_subs < numOfSubClasses) {
+							type = typeUri;
+							numOfSubClasses = num_subs;
+						}
+					}
+				}
+			}
+		}
+		
+		logger.debug(" = Found Individual rdf:type = "+type); 
+		return type;
+	}
+	
+	private int findNrofSubClasses (OntClass oc) {
+		String classUri = oc.getURI();
+		if (!numOfSubClasses.containsKey(classUri)) {
+			ExtendedIterator i = oc.listSubClasses();
+			numOfSubClasses.put(classUri, new Integer(i.toList().size()));
+		}
+		return numOfSubClasses.get(classUri).intValue();
+	}
+		
 	/** Small utility method to create an URI and throw an exeception appropriately.
 	 * 
 	 * @param uriStr
--- src/test/java/net/datamodel/soml/builder/TestBuilder.java
+++ src/test/java/net/datamodel/soml/builder/TestBuilder.java
@@ -7,12 +7,15 @@ import java.util.Iterator;
 
 import junit.framework.TestCase;
 import net.datamodel.soml.SemanticObject;
+import net.datamodel.soml.impl.SemanticObjectImpl;
 
 import org.apache.log4j.Logger;
 
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.util.FileManager;
 
 public class TestBuilder 
 extends TestCase 
@@ -21,16 +24,23 @@ extends TestCase
 	private static final Logger logger = Logger.getLogger(TestBuilder.class);
 
 	protected static String BASE_TEST_RESOURCE_DIR = "src/test/resources";
+	
+	private static final String BaseOntModelUri = "http://test.org/testThings.owl";
+	private static final OntModelSpec modelSpec = OntModelSpec.OWL_MEM;
+//	private static final OntModelSpec modelSpec = PelletReasonerFactory.THE_SPEC; // dont need a reasoner..we are just counting sub/super classes 
 
 	protected static String[] testModelFile = { 
 		BASE_TEST_RESOURCE_DIR + "/testBuilder1.rdf",
 //		BASE_TEST_RESOURCE_DIR + "/testBuilder2.rdf"
 	}; 
+	
+	// This rdf:type has a special handler we will check for
+	private static String SpecialInstanceURI = "http://test.org/testOps.owl#quantity1";
 
-	protected static OntModel[] testModels = new OntModel[testModelFile.length];
+	private static OntModel[] testModels = new OntModel[testModelFile.length];
 	
 	private static boolean isSetup = false;
-	private static SemanticObjectBuilder builder = new SemanticObjectBuilder();
+	private static SemanticObjectBuilder builder = null;
 	
 	@Override
 	protected void setUp() 
@@ -40,6 +50,9 @@ extends TestCase
 		
 		if (!isSetup) {
 
+			OntModel model = createOntModel(BaseOntModelUri);
+			builder = new TestBuilder().new ExtendedBuilder(model);
+	
 			// create the test model query
 			try {
 				for (int i = 0; i < testModelFile.length; i++) {
@@ -56,6 +69,20 @@ extends TestCase
 		
 	}
 
+	/** Build an OntModel for the passed query string.
+	 * 
+	 * @param queryStr
+	 * 
+	 * @return OntModel which represents the query 
+	 */
+	private static OntModel createOntModel (String ontoUri) {
+
+		OntModel queryModel = ModelFactory.createOntologyModel(modelSpec);
+		FileManager fm = FileManager.get();
+		queryModel.add(fm.loadModel(ontoUri,null, "RDF/XML-ABBREV"));
+		return queryModel;
+	}
+	
 	private static OntModel createOntModel (File ontoFile) 
 	throws FileNotFoundException 
 	{
@@ -73,12 +100,59 @@ extends TestCase
 				try {
 					SemanticObject so = builder.createSemanticObject(in); 
 					assertNotNull("Can create SO",so);
+					
+					// Check the class of the output object. For one object, we 
+					// had a special handler build it, otherwise, its SemanticObjectImpl
+					// class 
+					if (in.getURI().equals(SpecialInstanceURI)) {
+						assertEquals("Class for special object is correct", TestSemanticObject.class, so.getClass());
+					} else { 
+						assertEquals("Class for special object is correct", SemanticObjectImpl.class, so.getClass());
+					}
+					
 				} catch (SemanticObjectBuilderException e) {
 					fail(e.getLocalizedMessage());
 				}
 			}
 		}
 		
+	}
+	
+	/** test handler. to see if the extension mech is working.
+	 * 
+	 * @author thomas
+	 *
+	 */
+	class ExtendedBuilder 
+	extends SemanticObjectBuilder 
+	{
+		
+		String classURI = "http://test.org/testThings.owl#FloatQ3";
+		
+		public ExtendedBuilder (OntModel model) {
+			super(model); 
+			this.addHandler(classURI, new AHandler()); 
+		}
+		
+		class AHandler implements SemanticObjectHandler {
+
+			public SemanticObject create(SemanticObjectBuilder b, Individual in)
+					throws SemanticObjectBuilderException 
+			{
+				return new TestSemanticObject();
+			}
+			
+		}
+		
+	}
+	
+	class TestSemanticObject 
+	extends SemanticObjectImpl 
+	{
+		
+		TestSemanticObject() throws SemanticObjectBuilderException {
+			super(SemanticObjectBuilder.createURI("urn:no-uri-here"));
+		}
 	}
 
 }
